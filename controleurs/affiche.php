207e3caac3f435f1f7bfb2408b8e3f770fbaa9fc
Chap-z <cha.lacroix@hotmail.Fr>
2018-04-20 13:55:44 +0200

    affichage titre/genre/acteurs

--- controleurs/affiche.php
+++ controleurs/affiche.php
@@ -4,14 +4,21 @@ include '../php/model.php';
 
 $movies = getMovies();
 
+
 echo "<ul>";
 
 foreach ($movies as $movie) {
 
-    echo "<li>".$movie['titre'].$movie['genre']."</li>";
-
-
-
+    echo "<li>".$movie['titre']."<br>";
+    echo "Genre : ";
+    foreach ($movie['genre'] as $genre){
+        echo utf8_encode($genre[1])." ";
+    }
+    echo "<br>Acteurs :";
+    foreach ($movie['acteur'] as $acteur){
+        echo utf8_encode($acteur[1])." ".utf8_encode($acteur[2])." ";
+    }
+    echo "</li>";
 
 
 }
