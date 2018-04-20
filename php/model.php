5edb95b2aa1f3a1a11a9b6368411c2db4abcd998
Chap-z <cha.lacroix@hotmail.Fr>
2018-04-20 09:58:15 +0200

    genre ok

--- php/model.php
+++ php/model.php
@@ -1,6 +1,38 @@
 <?php 
 
+$dbo = new PDO('mysql:host=127.0.0.1;dbname=allo_cine', 'root', 'root');
 
+$arrayMovies = [];
+
+$stm = $dbo->prepare('SELECT titre_f, id_f FROM films');
+$stm->execute();
+
+$movies = $stm->fetchAll();
+$nbMovies = count($movies);
+
+// premièrement on boucle sur nos films
+foreach ($movies as $key => $row) {
+
+    // on stock dans un tableau le titre du film
+    $arrayMovies[$key] = [
+        'titre' => utf8_encode($row['titre_f'])
+    ];
+
+    // on request les genres du film en cours
+    $stg = $dbo->prepare('SELECT g.* FROM genres g INNER JOIN liaison_g_f lgf ON lgf.id_genre = g.id_g WHERE lgf.id_film = :id_film');
+    $stg->bindParam(':id_film', $row['id_f']);
+    $stg->execute();
+    $genres = $stg->fetchAll();
+
+    // on stock les genres récupérés dans le tableau précédents
+    $arrayMovies[$key]['genres'] = $genres;
+    
+}
+
+die;
+
+
+/* 
 $dbo = new PDO('mysql:host=127.0.0.1;dbname=allo_cine', 'root', 'root');
 
 $arrayMovies = [];
@@ -36,7 +68,7 @@ foreach ($movies as $row) {
             else {
                 break;
             }
-            //  
+              
         
             
          }
@@ -44,6 +76,6 @@ foreach ($movies as $row) {
       }
 
 
-}
+} */
 
 ?>
