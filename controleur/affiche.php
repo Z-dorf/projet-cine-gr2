d9ff09f0bbb2f48d31685341cbd4e666ed3d41bd
Chap-z <cha.lacroix@hotmail.Fr>
2018-04-23 11:24:51 +0200

    CA MARCHE

--- controleur/affiche.php
+++ controleur/affiche.php
@@ -1,19 +1,16 @@
 <?php 
-require 'vendor/autoload.php';
-
-include 'model/model.php';
+require_once ('vendor/autoload.php');
 
 $loader = new Twig_Loader_Filesystem('view');
-
 $twig = new Twig_Environment($loader,[
 
-    'cache' => false, // __DIR__ . 'tmp'
+    'cache' => false,
 
  ]);
 
-$movies = getMovies();
+require_once ('model/model.php');
 
-var_dump($movies);
+$movies = getMovies();
 
 //faire le render transmettre la vue au traitement
 $template = $twig->load('affiche.html');
@@ -21,17 +18,5 @@ $template = $twig->load('affiche.html');
 echo $template->render(array('movies' => $movies));
 
 var_dump ($movies);
-
-
-//Rendu du template
-
-
-
-
-
-
-
-
-
 
 ?>
