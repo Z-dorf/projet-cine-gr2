4059785a8df6bf2ba8f15468255aee953e638494
Chap-z <cha.lacroix@hotmail.Fr>
2018-04-24 09:56:25 +0200

    design v1

--- model/model.php
+++ model/model.php
@@ -5,10 +5,10 @@ function getMovies(){
     
    
     global $dbo;
-    $stm = $dbo->prepare('SELECT titre_f, id_f, description_f, annee_f FROM films');
-    $stm->execute();
+    $stms = $dbo->prepare('SELECT titre_f, id_f, description_f, annee_f FROM films');
+    $stms->execute();
 
-    $movies = $stm->fetchAll();
+    $movies = $stms->fetchAll();
 
     //on boucle sur nos films
     foreach ($movies as $row) {
